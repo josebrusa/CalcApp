37ff0cdad9c24ead4d2fb8989c4ce8c69281faf0
josebrusa <josebrusa@outlook.com>
2024-05-22 17:53:00 +0200

    configuracion inicial

---
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export const CalculatorScreen = () => {
+    return (
+        <View>
+            <Text>CalculatorScreen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
