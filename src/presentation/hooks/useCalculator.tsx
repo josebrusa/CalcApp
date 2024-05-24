606bf971b16a880f918a7bed9288759f17f1664e
josebrusa <josebrusa@outlook.com>
2024-05-24 18:24:43 +0200

    print number on windows

---
+++ src/presentation/hooks/useCalculator.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+
+
+export const useCalculator = () => {
+
+    const [ number, setNumber ] = useState('0')
+
+    const buildNumber = (numberString: string) => {
+
+        setNumber(number + numberString)
+
+    }
+
+
+
+
+
+
+    return {
+        //Properties
+        number,
+        //Method
+        buildNumber,
+    }
+}
